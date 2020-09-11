dad9d14dfa338b6e15ebbf38d9758270372ace59
Enaris <bularzkamil@gmail.com>
2020-09-11 09:12:07 +0200

    Upload service added

---
+++ Reader.API.Services/Services/IUploadService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Reader.API.Services.Services
+{
+    public interface IUploadService
+    {
+        Task<string> UploadImage(IFormFile request, Guid aspUserId);
+        Task<IEnumerable<string>> UploadImages(IEnumerable<IFormFile> files, Guid aspUserId);
+    }
+}
---
+++ Reader.API.Services/Services/UploadService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reader.API.Services.Services
+{
+    public class UploadService : IUploadService
+    {
+        const string FOLDER_PATH = "Static/Images/Covers";
+
+        public async Task<string> UploadImage(IFormFile request, Guid aspUserId)
+        {
+            if (request.Length <= 0)
+            {
+                return null;
+            }
+
+            CreateUserFolderIfDoesNotExist(aspUserId);
+            var filePath = GetFilePath(request.FileName, aspUserId);
+
+            using (var stream = System.IO.File.Create(filePath))
+            {
+                await request.CopyToAsync(stream);
+            }
+
+            return filePath;
+        }
+
+        public async Task<IEnumerable<string>> UploadImages(IEnumerable<IFormFile> files, Guid aspUserId)
+        {
+            var result = new List<string>(files.Count());
+            foreach (var f in files)
+            {
+                var uploadedImage = await UploadImage(f, aspUserId);
+                if (uploadedImage == null)
+                    continue;
+
+                result.Add(uploadedImage);
+            }
+
+            return result;
+        }
+
+        private string GetFilePath(string filename, Guid aspUserId)
+        {
+            var folderPath = FOLDER_PATH;
+            return $"{folderPath}/{aspUserId}/{filename}";
+        }
+
+        private void CreateUserFolderIfDoesNotExist(Guid aspUserId)
+        {
+            var userFolderPath = $"{FOLDER_PATH}/{aspUserId}";
+            if (!Directory.Exists(userFolderPath))
+                Directory.CreateDirectory(userFolderPath);
+        }
+    }
+}
