d78c65bd9b4edc7d28d49ca1f9ebfcaed24e17ab
Enaris <bularzkamil@gmail.com>
2020-09-11 10:51:12 +0200

    added optionsLog and readingSession repositories

---
+++ Reader.API.DataAccess/Repositories/OptionsLogRepository.cs
@@ -0,0 +1,16 @@
+﻿using Reader.API.DataAccess.Context;
+using Reader.API.DataAccess.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reader.API.DataAccess.Repositories
+{
+    public class OptionsLogRepository : BaseRepository<OptionsLog>, IOptionsLogRepository
+    {
+        public OptionsLogRepository(ReaderContext context) : base(context)
+        {
+        }
+
+    }
+}
---
+++ Reader.API.DataAccess/Repositories/ReadingSessionRepository.cs
@@ -0,0 +1,15 @@
+﻿using Reader.API.DataAccess.Context;
+using Reader.API.DataAccess.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reader.API.DataAccess.Repositories
+{
+    public class ReadingSessionRepository : BaseRepository<ReadingSession>, IReadingSessionRepository
+    {
+        public ReadingSessionRepository(ReaderContext context) : base(context)
+        {
+        }
+    }
+}
